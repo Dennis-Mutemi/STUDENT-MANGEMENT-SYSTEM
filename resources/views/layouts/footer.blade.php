99eb254fb58db8b91e3e2777c4d19f8ca4914421
Dennis Mutemi <denismutemi84@gmail.com>
2024-03-24 18:55:59 +0300

    First commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+
+
+<footer class="bg-amber-200 fixed bottom-0 left-0 z-20 w-full p-4 border-t shadow  md:p-6 dark:border-gray-600">    
+    <div class="font-extralight text-right">
+        Designed by Nislaudia Technologies
+</div>
+</footer>
